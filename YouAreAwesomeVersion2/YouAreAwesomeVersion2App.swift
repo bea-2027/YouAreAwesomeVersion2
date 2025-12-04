7bc29811e3649e878f68b4036d6bd10d2378eeb0
Beatriz <btovar0002@mymail.lausd.net>
2025-12-04 11:20:39 -0800

    Initial Commit

---
+++ YouAreAwesomeVersion2/YouAreAwesomeVersion2App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeVersion2App.swift
+//  YouAreAwesomeVersion2
+//
+//  Created by TOVAR, BEATRIZ on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeVersion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
